4a5ef9988c670e8b4cb8da26a8959a94d469f507
Asif Azimi <iasifazimi@outlook.com>
2022-09-15 15:14:26 +0430

    Initialize-project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>react shopping cart</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
